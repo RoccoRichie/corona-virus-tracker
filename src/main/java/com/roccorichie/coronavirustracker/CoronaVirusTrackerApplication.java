7cf4ab92f4eebdc8bef2328ec397ef39853b74ce
Richie <richieloughran@gmail.com>
2020-02-29 14:43:24 +0000

    Added Apache CSV parser, Location Stat Model Via withFirstRecordAsHeader, sysout to console

--- src/main/java/com/roccorichie/coronavirustracker/CoronaVirusTrackerApplication.java
+++ src/main/java/com/roccorichie/coronavirustracker/CoronaVirusTrackerApplication.java
@@ -2,8 +2,10 @@ package com.roccorichie.coronavirustracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoronaVirusTrackerApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/roccorichie/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/roccorichie/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,13 +1,22 @@
 package com.roccorichie.coronavirustracker.services;
 
+import com.roccorichie.coronavirustracker.models.LocationStats;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 // Make this a spring service
 @Service
@@ -15,15 +24,41 @@ public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
 
-    //Execute this at start
+    private List<LocationStats> allStats = new ArrayList<>();
+
+    //Execute this at start - When you construct an instance of this class, execute this method
+    // Scheduler runs as a cron say every day
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
+
+        // Once this list is complied we will add it to the allStats list
+        // => Avoids throwing exceptions when user requests allStats
+        List<LocationStats> newStats = new ArrayList<>();
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(VIRUS_DATA_URL))
                 .build();
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
         // Print to console
-        System.out.println(httpResponse.body());
+        //System.out.println(httpResponse.body());
+
+        // Convert the string into a String Reader and parse it
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        // header Province/State,Country/Region,Lat,Long,1/22/20,1/23/20,
+        // sample data: Beijing,Mainland China,40.1824,116.4142,14,
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            LocationStats locationStats = new LocationStats();
+            locationStats.setState(record.get("Province/State"));
+            locationStats.setCountry(record.get("Country/Region"));
+            locationStats.setLatestTotalCases(Integer.parseInt(record.get(record.size() -1)));
+
+            // Print locationStat
+            System.out.println(locationStats);
+            newStats.add(locationStats);
+        }
+        this.allStats = newStats;
     }
 }
