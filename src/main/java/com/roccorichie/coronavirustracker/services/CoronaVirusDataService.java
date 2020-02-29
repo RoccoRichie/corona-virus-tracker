4f4593f16e4ac2e622cc453e0c992e4ac2609f91
Richie <richieloughran@gmail.com>
2020-02-29 13:48:21 +0000

    First Commit - Java Brains Tut

---
+++ src/main/java/com/roccorichie/coronavirustracker/services/CoronaVirusDataService.java
@@ -0,0 +1,29 @@
+package com.roccorichie.coronavirustracker.services;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+// Make this a spring service
+@Service
+public class CoronaVirusDataService {
+
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+
+    //Execute this at start
+    @PostConstruct
+    public void fetchVirusData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATA_URL))
+                .build();
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        // Print to console
+        System.out.println(httpResponse.body());
+    }
+}
